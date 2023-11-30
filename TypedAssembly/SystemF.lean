14be57e06930407da9f25103a093ab4eb4ec3fcb
Fady Adal <2masadel@gmail.com>
2023-11-29 23:02:02 -0600

    pre hoas

--- TypedAssembly/SystemF.lean
+++ TypedAssembly/SystemF.lean
@@ -70,7 +70,7 @@ namespace Typ
   | `( ⋆⟪ ∀. $t:typ ⟫) => `(Typ.for_all ⋆⟪ $t ⟫)
   | `( ⋆⟪ ( $t )  ⟫ ) => `(⋆⟪ $t ⟫)
 
-  def polyidty : ∅⋆ ⊢⋆ := ⋆⟪ ∀. ♯0 → ♯0 ⟫
+  def polyidt : ∅⋆ ⊢⋆ := ⋆⟪ ∀. ♯0 → ♯0 ⟫
 end Typ
 open Typ
 
@@ -80,43 +80,77 @@ open Typ
 @[reducible]
 def Rent Δ₁ Δ₂ := Δ₁✓ → Δ₂✓
 
-/-- lift⋆ takes a Ren⋆ and returns a different Ren⋆ that operates in a typing context
-    where the meaning of the first typvar is unaffected but all later typ variable
-    are shifted by 1 position -/
 @[reducible]
 def liftt {Δ₁ Δ₂} : Rent Δ₁ Δ₂ → Rent ⊹Δ₁ ⊹Δ₂ 
   | _, .here => .here
   | r, .there a => .there (r a)
 
-/-- ren⋆ takes a Ren⋆ and a typ and moves all the typ variables in that typ to the
+theorem lifttfunc_id : ∀ {Δ}, @liftt Δ Δ id = id := by
+  intros Δ 
+  unfold liftt
+  funext x
+  split <;> simp
+
+theorem lifttfunc_comp : ∀ {Δ₁ Δ₂ Δ₃} {rt₁ : Rent Δ₁ Δ₂} {rt₂ : Rent Δ₂ Δ₃},
+  @liftt Δ₁ Δ₃ (rt₂ ∘ rt₁) = liftt rt₂ ∘ liftt rt₁ := by 
+  intros Δ₁ Δ₂ Δ₃ rt₁ rt₂
+  unfold liftt
+  funext x
+  split <;> simp
+
+/-- ren⋆ takes a Ren⋆ and a typ and applies it to that typ, moving it to the
     new context -/
 @[reducible]
 def rent {Δ₁ Δ₂} : Rent Δ₁ Δ₂ → Δ₁⊢⋆ → Δ₂⊢⋆
   | _, .int => .int
-  | r, .var a => .var (r a)
-  | r, .arrow a b => .arrow (rent r a) (rent r b)
-  | r, .prod a b => .prod (rent r a) (rent r b)
-  | r, .for_all b => .for_all (rent (liftt r) b)
+  | rt, .var x => .var (rt x)
+  | rt, .arrow e₁ e₂ => .arrow (rent rt e₁) (rent rt e₂)
+  | rt, .prod e₁ e₂ => .prod (rent rt e₁) (rent rt e₂)
+  | rt, .for_all e => .for_all (rent (liftt rt) e)
 
-/-- weaken⋆ shifts over all typ variables in a variable by 1 position,
-    e.g. ⋆⟪ ♯0 → ♯0 ⟫ : ⊹∅⋆ ⊢⋆ becomes ⋆⟪ ♯1 → ♯1 ⟫ : ⊹⊹∅⋆ ⊢⋆ -/
+theorem rent_id : ∀ {Δ} {t : Δ ⊢⋆}, rent id t = t := by
+  intros Δ t
+  induction t with simp [*]
+  | for_all t' t'_ih =>
+    rename_i Δ'
+    rw [lifttfunc_id]
+    assumption
+
+theorem rent_comp : ∀ {Δ₁ Δ₂ Δ₃} {rt₁ : Rent Δ₁ Δ₂} {rt₂ : Rent Δ₂ Δ₃} {t : Δ₁ ⊢⋆},
+  rent (rt₂ ∘ rt₁) t = rent rt₂ (rent rt₁ t) := by
+  intros Δ₁ Δ₂ Δ₃ rt₁ rt₂ t
+  induction t generalizing rt₂ Δ₂ Δ₃ with simp [*]
+  | for_all t' t'_ih => 
+    rename_i Δ₁'
+    rw [lifttfunc_comp]
+    exact (@t'_ih ⊹Δ₂ ⊹Δ₃ (liftt rt₁) (liftt rt₂))
+
+/-- weaken⋆ is a Ren⋆ that shifts over all free
+    typ variables in a typ by 1 position,
+    e.g. ⋆⟪ ∀. ♯0 → ♯1 ⟫ : ⊹∅⋆ ⊢⋆ becomes ⋆⟪ ∀. ♯0 → ♯2 ⟫ : ⊹⊹∅⋆ ⊢⋆ -/
 @[reducible]
-def weakent {c} : c⊢⋆ → (⊹c)⊢⋆ := rent TypVar.there
+def weakent {Δ} : Δ⊢⋆ → (⊹Δ)⊢⋆ := rent TypVar.there
+
+example : weakent (⋆⟪ ♯0 → ♯1 ⟫ : ⊹⊹∅⋆ ⊢⋆) = ⋆⟪ ♯1 → ♯2 ⟫ := rfl
+example : weakent (⋆⟪ ∀. ♯0 → ♯1 ⟫ : ⊹∅⋆ ⊢⋆) = ⋆⟪ ∀. ♯0 → ♯2 ⟫ := rfl
+
 
 /-- A Subs⋆ Δ₁ Δ₂ is a function that takes a typ variable and returns 
     a typ (in a possibly modified context) that should be in its place -/
 def Subst Δ₁ Δ₂ := Δ₁✓ → Δ₂⊢⋆
 
-/-- lifts⋆ takes a Subs⋆ and returns a different Subs⋆ that operates in a typing context where
-    the meaning of the first typevar is unaffected but all later type variables are shifted by 1 position -/
-@[reducible, simp]
+/-- lifts⋆ takes a Subs⋆ and returns a different Subs⋆ that operates
+    in a typing context where
+    the meaning of the first typevar is unaffected but all later
+    type variables are shifted down by 1 position -/
+@[reducible]
 def liftst {Δ₁ Δ₂} : Subst Δ₁ Δ₂ → Subst ⊹Δ₁ ⊹Δ₂
   | _, .here => .var .here
   | s, .there a => weakent (s a)
 
-/-- subs⋆ takes a Subs⋆ and a type and replaces the relevant type variable with that type, 
-    returning a new type (in a possibly modified context) -/
-@[reducible, simp]
+/-- subs⋆ takes a Subs⋆ and a type and replaces the relevant type variable with that
+    type, returning a new type (in a possibly modified context) -/
+@[reducible]
 def subst {Δ₁ Δ₂} : Subst Δ₁ Δ₂ → Δ₁⊢⋆ → Δ₂⊢⋆
   | _, .int => .int
   | s, .var a => s a
@@ -139,37 +173,25 @@ macro b:term:80 "[" a:term:80 "]⋆" : term =>
 
 theorem «.var is identity subst» : ∀ (t : Δ⊢⋆), subst .var t = t := by
   intros t
-  induction t <;>
-    try rfl 
-  case arrow =>
-    simp
-    constructor <;> assumption
-  case prod =>
-    simp
-    constructor <;> assumption
-
-  case for_all Δ' t' t_ih =>
-    simp only [subst]
-    congr
+  induction t with simp [*]
+  | for_all t' t'_ih =>
+    rename_i Δ'
     have : @liftst Δ' Δ' Typ.var = Typ.var := by
-          simp
+          unfold liftst
           funext x 
           cases x <;> rfl
     rw [this]
     assumption
 
 example : ⋆⟪ ♯0 → ♯0 ⟫[.int]⋆ = (⋆⟪ int → int ⟫ : Typ ∅⋆) := rfl
+example : ⋆⟪ ∀. ♯0 → ♯0 ⟫[.int]⋆ = (⋆⟪ ∀. ♯0 → ♯0 ⟫ : Typ ∅⋆) := rfl
+example : ⋆⟪ ∀. ♯0 → ♯1 ⟫[.int]⋆ = (⋆⟪ ∀. ♯0 → int ⟫ : Typ ∅⋆) := rfl
 
-inductive Prim where
-  | plus
-  | minus
-  | times
-  deriving Repr, BEq, DecidableEq
 
 inductive Ctx : TCtx → Type where
   | nil : Ctx Δ
   | snoc_kind : Ctx Δ → Ctx ⊹Δ
-  | snoc_typ : Ctx Δ → c⊢⋆ → Ctx Δ
+  | snoc_typ : Ctx Δ → Δ⊢⋆ → Ctx Δ
 
 namespace Ctx
   infixl:50 " ‘ " => Ctx.snoc_typ
@@ -178,15 +200,22 @@ end Ctx
 open Ctx
 
 inductive Lookup : Ctx Δ → Δ⊢⋆ → Type where
-| here : Lookup (Γ ‘ t) t
-| there : Lookup Γ t → Lookup (Γ ‘ t') t
-| move : Lookup Γ t → Lookup Γ.snoc_kind (weakent t)
+  | here  {Γ : Ctx Δ} {t : Δ⊢⋆} : Lookup (Γ ‘ t) t
+  | there {Γ : Ctx Δ} {t : Δ⊢⋆} : Lookup Γ t → Lookup (Γ ‘ t') t
+  | move  {Γ : Ctx Δ} {t : Δ⊢⋆} : Lookup Γ t →
+                                  Lookup Γ.snoc_kind (weakent t)
 deriving Repr
 
 namespace Lookup
   infix:40 " ∋ " => Lookup
 end Lookup
 open Lookup
+
+inductive Prim where
+  | plus
+  | minus
+  | times
+  deriving Repr, BEq, DecidableEq
 
 inductive Term : (Δ : TCtx) → Ctx Δ → Typ Δ → Type where
   | int  {Γ : Ctx Δ} : Int → Term Δ Γ .int
@@ -195,14 +224,14 @@ inductive Term : (Δ : TCtx) → Ctx Δ → Typ Δ → Type where
   | fix  {Γ : Ctx Δ} {t₁ t₂ : Δ⊢⋆} : Term Δ (Γ ‘ t₁ ‘ ⋆⟪ !t₁ → !t₂ ⟫) t₂ → Term Δ Γ ⋆⟪ !t₁ → !t₂ ⟫
   | app  {Γ : Ctx Δ} {t₁ t₂ : Δ⊢⋆} : Term Δ Γ ⋆⟪ !t₁ → !t₂ ⟫ → Term Δ Γ t₁ → Term Δ Γ t₂
 
-  | Λ    {Γ : Ctx Δ} {t : (⊹Δ)⊢⋆} : Term ⊹Δ Γ.snoc_kind t → Term Δ Γ ⋆⟪ ∀. !t ⟫
+  | Λ    {Γ : Ctx Δ} {t  : (⊹Δ)⊢⋆} : Term ⊹Δ Γ.snoc_kind t → Term Δ Γ ⋆⟪ ∀. !t ⟫
   | sub  {Γ : Ctx Δ} {t' : (⊹Δ)⊢⋆} : Term Δ Γ ⋆⟪ ∀. !t' ⟫ → (t : Δ⊢⋆) → Term Δ Γ (t'[t]⋆)
 
   | prim {Γ : Ctx Δ} : Term Δ Γ .int → Prim → Term Δ Γ .int → Term Δ Γ .int
   | pair {Γ : Ctx Δ} : Term Δ Γ t₁ → Term Δ Γ t₂ → Term Δ Γ ⋆⟪ !t₁ × !t₂ ⟫
-  | fst {Γ : Ctx Δ} : Term Δ Γ ⋆⟪ !t₁ × !t₂ ⟫ → Term Δ Γ t₁
-  | snd {Γ : Ctx Δ} : Term Δ Γ ⋆⟪ !t₁ × !t₂ ⟫ → Term Δ Γ t₂
-  | if0  {Γ : Ctx Δ} {t : Δ⊢⋆}: Term Δ Γ .int → Term Δ Γ t → Term Δ Γ t → Term Δ Γ t
+  | fst  {Γ : Ctx Δ} : Term Δ Γ ⋆⟪ !t₁ × !t₂ ⟫ → Term Δ Γ t₁
+  | snd  {Γ : Ctx Δ} : Term Δ Γ ⋆⟪ !t₁ × !t₂ ⟫ → Term Δ Γ t₂
+  | if0  {Γ : Ctx Δ} {t : Δ⊢⋆} : Term Δ Γ .int → Term Δ Γ t → Term Δ Γ t → Term Δ Γ t
   deriving Repr
 
 namespace Term 
@@ -263,7 +292,6 @@ def fact : ∅⋆ ∣ ∅ ⊢ ⋆⟪ int → int ⟫ :=
   Term.fix (.if0 (.var (.there .here))
             (.int 1)
             (.prim (.var (.there .here)) .times (.app (.var .here) (Term.prim (.var (.there .here)) .minus (.int 1)))))
-#eval fact
 
 def fact' : ∅⋆ ∣ ∅ ⊢ ⋆⟪ int → int ⟫ := 
   ⟪ λ. if0 #1 then
@@ -272,41 +300,119 @@ def fact' : ∅⋆ ∣ ∅ ⊢ ⋆⟪ int → int ⟫ :=
          #1 * (#0 (#1 - 1))
        end ⟫
 
-#eval fact'
-
 theorem fact_eq_fact' : fact = fact' := rfl
 
 def sixfact := ⟪ !fact 6 ⟫
 
 def freeid : ⊹∅⋆ ∣ ∅.snoc_kind ⊢ ⋆⟪ ♯0 → ♯0 ⟫ :=
- ⟪ λ. #1 ⟫
+  ⟪ λ. #1 ⟫
 
-def intid := ⟪ (Λ. !freeid)[int] ⟫
-#eval intid
+def intid : ∅⋆ ∣ ∅ ⊢ ⋆⟪ int → int ⟫ :=
+  ⟪ (Λ. !freeid)[int] ⟫
+
+def convni {Δ} {Γ : Ctx Δ} {t₁ t₂ : Δ ⊢⋆} : t₁ = t₂ → Γ ∋ t₁ → Γ ∋ t₂ := by
+  intros hp x
+  rw [←hp]
+  exact x
+
+def convent {Δ} {Γ : Ctx Δ} {t₁ t₂ : Δ ⊢⋆} : t₁ = t₂ → (Δ ∣ Γ ⊢ t₁) → (Δ ∣ Γ ⊢ t₂) := by
+  intros hp e
+  rw [←hp]
+  exact e
 
 @[reducible]
 def Ren : ∀ {Δ₁ Δ₂}, Ctx Δ₁ → Ctx Δ₂ → Rent Δ₁ Δ₂ → Type := 
-  fun Γ₁ Γ₂ rt => {a : _ ⊢⋆} → Γ₁ ∋ a → Γ₂ ∋ rent rt a
+  fun Γ₁ Γ₂ rt => {t : _ ⊢⋆} → Γ₁ ∋ t → Γ₂ ∋ rent rt t
 
 
 @[reducible]
 def lift : ∀ {Δ₁ Δ₂ Γ₁ Γ₂} {rt : Rent Δ₁ Δ₂}, 
-          Ren Γ₁ Γ₂ rt → {b : Δ₁ ⊢⋆} → Ren (Γ₁ ‘ b) (Γ₂ ‘ rent rt b) rt := 
-    fun r _ b x => 
+          Ren Γ₁ Γ₂ rt → {t : Δ₁ ⊢⋆} →
+          Ren (Γ₁ ‘ t) (Γ₂ ‘ rent rt t) rt := 
+    fun r _ _ x => 
       match x with
       | .here => .here
-      | .there x' => .there (r x')
+      | .there x => .there (r x)
 
-def convninf {Δ Γ} {a₁ a₂ : Δ ⊢⋆} :  
-              a₁ = a₂ → Γ ∋ a₁ → Γ ∋ a₂
-      | rfl, x => x
+theorem «weakent liftt»:
+  ∀ {Δ₁ Δ₂} {rt : Rent Δ₁ Δ₂} {t : Δ₁⊢⋆},
+    rent (liftt rt) (weakent t) = weakent (rent rt t) := by
+  intros Δ₁ Δ₂ rt t
+  induction t generalizing Δ₂ with simp [*]
+  | for_all t' t'_ih => 
+    rename_i Δ'
+    admit
+
 
 @[reducible]
-def tlift : ∀ {Δ₁ Δ₂ Γ₁ Γ₂} {rt : Rent Δ₁ Δ₂}, 
-            Ren Γ₁ Γ₂ rt → {b : Δ₁ ⊢⋆} → Ren Γ₁.snoc_kind Γ₂.snoc_kind (liftt rt) := by
-    intros Δ₁ Δ₂ Γ₁ Γ₂ rt r b a v
-    cases v with
-    | move v => admit
+def tlift : ∀ {Δ₁ Δ₂} {Γ₁ : Ctx Δ₁} {Γ₂ : Ctx Δ₂}
+              {rt : Rent Δ₁ Δ₂}, 
+            Ren Γ₁ Γ₂ rt →
+            Ren Γ₁.snoc_kind Γ₂.snoc_kind (liftt rt) :=
+    fun r t x =>  
+      match x with
+      | .move x' => by 
+        rename_i Δ₁ Δ₂ Γ₁ Γ₂ rt t'
+        apply convni
+        case t₁ =>
+          have t₂ := weakent (rent rt t')
+          exact t₂
+        · dsimp
+          apply Eq.symm
+
+          apply «weakent liftt»
+        · simp
+          exact (.move (r x'))
+
+def ren : ∀ {Δ₁ Δ₂ Γ₁ Γ₂} {rt : Rent Δ₁ Δ₂},
+          Ren Γ₁ Γ₂ rt → {t : Δ₁ ⊢⋆} → (Δ₁ ∣ Γ₁ ⊢ t) → 
+          (Δ₂ ∣ Γ₂ ⊢ (rent rt t)) :=
+    fun r t a => 
+    match a with
+    | .int n => .int n
+    | .var x => .var (r x)
+
+    | .fix e => .fix (ren (lift (lift r)) e)
+    | .app e₁ e₂ => .app (ren r e₁) (ren r e₂)
+
+    | .Λ e => .Λ (ren (tlift r) e)
+    | .sub e t'  => by
+      rename_i Δ₁ Δ₂ Γ₁ Γ₂ rt t''
+      have t := Term.sub (ren r e) (rent rt t')
+      admit
+ 
+      -- exact 
+    | .prim e₁ p e₂ => .prim (ren r e₁) p (ren r e₂)
+    | .pair e₁ e₂ => .pair (ren r e₁) (ren r e₂)
+    | .fst e => .fst (ren r e)
+    | .snd e => .snd (ren r e)
+    | .if0 e₁ e₂ e₃ => .if0 (ren r e₁) (ren r e₂) (ren r e₃)
+
+def weaken {Δ} {Γ : Ctx Δ} {t₁ t₂ : Δ ⊢⋆} :
+  (Δ ∣ Γ ⊢ t₁) → (Δ ∣ (Γ ‘ t₂) ⊢ t₁) :=
+    fun e => convent rent_id (ren (convni (Eq.symm rent_id) ∘ .there) e)
+
+def tweaken {Δ} {Γ : Ctx Δ} {t : Δ ⊢⋆} :
+  (Δ ∣ Γ ⊢ t) → (⊹Δ ∣ Γ.snoc_kind ⊢ weakent t) := ren .move
+
+def Subs {Δ₁ Δ₂} : (Γ₁ : Ctx Δ₁) → (Γ₂ : Ctx Δ₂) →  Subst Δ₁ Δ₂ → Type :=
+  fun Γ₁ Γ₂ st => {t : _ ⊢⋆} → Γ₁ ∋ t → (_ ∣ Γ₂ ⊢ (subst st t))
+
+def lifts {Δ₁ Δ₂} {Γ₁ : Ctx Δ₁} {Γ₂ : Ctx Δ₂}
+          {st : Subst Δ₁ Δ₂} : Subs Γ₁ Γ₂ st → 
+          {t' : _ ⊢⋆} → Subs (Γ₁ ‘ t') (Γ₂ ‘ (subst st t')) st :=
+    fun s _ _ x => 
+      match x with
+      | .here => .var .here
+      | .there x' => weaken (s x')
+
+def tlifts {Δ₁ Δ₂} {Γ₁ : Ctx Δ₁} {Γ₂ : Ctx Δ₂}
+           {st : Subst Δ₁ Δ₂} : Subs Γ₁ Γ₂ st → 
+           Subs Γ₁.snoc_kind Γ₂.snoc_kind (liftst st) :=
+    fun s t x => 
+      match x with
+      | .move x' => by
+        admit
 
 abbrev interp : Prim → (Int → Int → Int)
   | .plus => Int.add
@@ -314,6 +420,7 @@ abbrev interp : Prim → (Int → Int → Int)
   | .times => Int.mul
 
 inductive Step {Δ Γ} : (Δ ∣ Γ ⊢ t) → (Δ ∣ Γ ⊢ t) → Prop
+
   | prim_exec  : Step (.prim (.int n₁) p (.int n₂)) (.int (interp p n₁ n₂))
   | prim_left  : Step a a' → Step (.prim a p b) (.prim a' p b)
   | prim_right : Step b b' → Step (.prim (.int n) p b) (.prim (.int n) p b')
